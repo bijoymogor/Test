105352598806efdd72ba911876c2fe990c52c7f9
Bijoy Mogor <bijoymogor@gmail.com>
2022-05-24 22:28:42 +0530

    24-05 drum work completed

--- index.js
+++ index.js
@@ -5,6 +5,57 @@ document.querySelectorAll(".drum")[i].addEventListener("click", handleClick);
 }
 
 
+document.addEventListener("keydown",function(event){
+    handleKeyPress1(event['key']);
+})
+
+
+function handleKeyPress1(ch){
+    button_pressed = ch;
+    if(button_pressed == 'w'){
+var audio  = new Audio('sounds\\tom-1.mp3');
+
+}
+
+else if(button_pressed == 'a')
+{
+var audio  = new Audio('sounds\\tom-2.mp3');
+
+}
+
+else if(button_pressed == 's'){
+var audio  = new Audio('sounds\\tom-3.mp3');
+
+}
+
+else if(button_pressed == 'd'){
+var audio  = new Audio('sounds\\tom-4.mp3');
+
+}
+
+else if(button_pressed == 'j'){
+var audio  = new Audio('sounds\\snare.mp3');
+
+}
+
+else if(button_pressed == 'k'){
+var audio  = new Audio('sounds\\crash.mp3');
+
+}
+
+else if(button_pressed == 'l'){
+var audio  = new Audio('sounds\\crash.mp3');
+
+}
+
+audio.play();
+
+
+}
+
+
+
+
 function handleClick(){
 
 button_pressed = this.innerHTML
